f16e465ee1369f10baf66ee4376498d9041be887
RuanGom <ruangomes8597@gmail.com>
2023-10-08 17:19:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express'
+import usuarioController from './controller/usuarioController.js'
+
+
+const server = express();
+server.use(cors());
+server.use(express.json())
+
+server.use(usuarioController);
+
+server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`));
+
